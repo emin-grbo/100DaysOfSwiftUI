534e73dd109c6411ca80b284c1b2eb32050d5348
emin.grbo <emin.roblack@gmail.com>
2019-10-22 22:40:59 +0200

    Day 29 innitial commit

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by roblack on 10/22/19.
+//  Copyright © 2019 roblack. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
