3285ef9bffc4dda3af44b38f1cdabd856f5c1e9d
emin.grbo <emin.roblack@gmail.com>
2019-12-05 19:57:25 +0100

    Day 73

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -14,47 +14,14 @@ struct ContentView: View {
     
     @State private var isUnlocked = false
 
-    @State private var centerCoordinate = CLLocationCoordinate2D()
-    @State private var locations = [CodableMKPointAnnotation]()
-    
-    @State private var selectedPlace: MKPointAnnotation?
-    @State private var showingPlaceDetails = false
+    @State private var isShowingError = false
+    @State private var errorMessage = ""
 
-    @State private var showingEditScreen = false
-    
     var body: some View {
         
         ZStack {
             if isUnlocked {
-            MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
-                .edgesIgnoringSafeArea(.all)
-            Circle()
-                .fill(Color.blue)
-                .opacity(0.3)
-                .frame(width: 32, height: 32)
-            
-            VStack {
-                Spacer()
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        let newLocation = CodableMKPointAnnotation()
-                        newLocation.coordinate = self.centerCoordinate
-                        newLocation.title = "Example location"
-                        self.locations.append(newLocation)
-                        self.selectedPlace = newLocation
-                        self.showingEditScreen = true
-                    }) {
-                        Image(systemName: "plus")
-                    }
-                    .padding()
-                    .background(Color.black.opacity(0.75))
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .clipShape(Circle())
-                    .padding(.trailing)
-                }
-            }
+            UnlockedMapView()
             } else {
                     Button("Unlock Places") {
                         self.authenticate()
@@ -65,17 +32,9 @@ struct ContentView: View {
                     .clipShape(Capsule())
                 }
         }
-        .alert(isPresented: $showingPlaceDetails) {
-            Alert(title: Text(selectedPlace?.title ?? "Unknown"), message: Text(selectedPlace?.subtitle ?? "Missing place information."), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
-                self.showingEditScreen = true
-            })
+        .alert(isPresented: $isShowingError) {
+            Alert(title: Text("Error has occured"), message: Text(errorMessage), dismissButton: .default(Text("OK")))
         }
-        .sheet(isPresented: $showingEditScreen, onDismiss: saveData) {
-            if self.selectedPlace != nil {
-                EditView(placemark: self.selectedPlace!)
-            }
-        }
-        .onAppear(perform: loadData)
     }
     
     func authenticate() {
@@ -93,38 +52,14 @@ struct ContentView: View {
                     if success {
                         self.isUnlocked = true
                     } else {
-                        // there was a problem
+                        self.isShowingError.toggle()
+                        self.errorMessage = authenticationError?.localizedDescription ?? "Unknown error"
                     }
                 }
             }
         } else {
-            // no biometrics
-        }
-    }
-    
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
-    }
-    
-    func loadData() {
-        let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
-
-        do {
-            let data = try Data(contentsOf: filename)
-            locations = try JSONDecoder().decode([CodableMKPointAnnotation].self, from: data)
-        } catch {
-            print("Unable to load saved data.")
-        }
-    }
-    
-    func saveData() {
-        do {
-            let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
-            let data = try JSONEncoder().encode(self.locations)
-            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
-        } catch {
-            print("Unable to save data.")
+            isShowingError.toggle()
+            errorMessage = "No Biometrics"
         }
     }
     
